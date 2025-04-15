72a2141415886ceff69a9f67cf1ad9b23c990159
muhire <dushimiyimanaelisa@gmail.com>
2025-04-15 21:03:13 +0200

    fix some change

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const dbName = process.env.DB_NAME;
 
 // Define CORS options
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://elisa-dev-portfolio.vercel.app"],
+  origin: ["http://localhost:5173", "https://elisa-dev-portfolio.vercel.app","https://dtechel.web.app/"],
   optionsSuccessStatus: 200,
   credentials: true,
 };
