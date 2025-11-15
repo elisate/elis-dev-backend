ef7d93de91cf2fac5dc9a05563740bc2889352f6
elisate <dushimiyimanaelisa@gmail.com>
2025-11-15 16:31:18 +0200

    fix some issues in codes

--- utils/sendemail.js
+++ utils/sendemail.js
@@ -1,7 +1,9 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
+
 dotenv.config();
-// Create a Nodemailer transporter
+
+// Create a reusable Nodemailer transporter
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -9,29 +11,33 @@ const transporter = nodemailer.createTransport({
     pass: process.env.EMAIL_PASS,
   },
   tls: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // Allows self-signed certificates
   },
 });
 
-// Function to send email
+/**
+ * Sends an email using Nodemailer
+ * @param {string} to - Recipient email address
+ * @param {string} subject - Email subject
+ * @param {string} htmlContent - Email content in HTML
+ * @returns {Promise<boolean>} - Returns true if email sent successfully, false otherwise
+ */
 const sendEmail = async (to, subject, htmlContent) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    html: htmlContent,
+  };
+
   try {
-    // Email configuration
-    const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: to,
-      subject: subject,
-      html: htmlContent, // Use 'html' instead of 'text' for HTML content
-    };
-
-    // Send email
     await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully.");
-
-    return true; // Return true if email is sent successfully
+    console.log(`Email sent successfully to: ${to}`);
+    return true;
   } catch (error) {
-    console.log("Error sending email: ", error.message);
-    return false; // Return false if there's an error sending email
+    console.error(`Error sending email to ${to}:`, error.message);
+    return false;
   }
 };
+
 export default sendEmail;
